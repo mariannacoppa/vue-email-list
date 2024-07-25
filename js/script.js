2be6852cc3f0918b828d622b24ce6ead20df8462
mariannacoppa <densimetra@gmail.com>
2024-07-25 16:19:58 +0200

    [UPD] created v-for cycle

--- js/script.js
+++ js/script.js
@@ -4,15 +4,22 @@ createApp({
     data() {
         return {
             email: null,
-            arrayResult: null
+            mail: null
         }
     },
     methods: {
         getEmail() {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
                 this.email = resp.data.response;
-                console.log(this.arrayResult);
+                console.log(this.email);
             });
+        },
+        getTenEmails() {
+            for (let i = 0; i < 11; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
+                    this.mail.push(resp.data.response);
+                });
+            }
         }
     }
 }).mount('#app');
