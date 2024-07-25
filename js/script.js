ad84a7865fb966a6e4dc920e37e9c7d526079ec2
mariannacoppa <densimetra@gmail.com>
2024-07-25 17:03:10 +0200

    [UPD] deleted getEmail function FINE

--- js/script.js
+++ js/script.js
@@ -8,12 +8,6 @@ createApp({
         }
     },
     methods: {
-        getEmail() {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
-                this.email = resp.data.response;
-                console.log(this.email);
-            });
-        },
         getTenEmails() {
             for (let i = 0; i < 10; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
